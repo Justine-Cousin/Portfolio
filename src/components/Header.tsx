1730a4c0e6c2a7720625adce9d024e8d6005c35b
JustineC31 <justine-cousin@hotmail.com>
2024-12-28 18:14:14 +0100

    style: header css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,27 @@ import about from "../assets/Images/about.svg";
 import contact from "../assets/Images/contact.svg";
 import home from "../assets/Images/home.svg";
 import projects from "../assets/Images/project.svg";
+import "../styles/Header.css";
 
 export default function Header() {
 	return (
-		<div>
-			<h1>Justine Cousin</h1>
-			<nav>
-				<li>
-					<img src={home} alt="Accueil" />
+		<div className="header-page">
+			<h1 className="header-title">Justine Cousin</h1>
+			<nav className="header-nav">
+				<li className="header-logo-home-container">
+					<img className="header-logo-home" src={home} alt="Accueil" />
 					<p>Accueil</p>
 				</li>
-				<li>
-					<img src={about} alt="À propos" />
+				<li className="header-logo-about-container">
+					<img className="header-logo-about" src={about} alt="À propos" />
 					<p>A propos</p>
 				</li>
-				<li>
-					<img src={projects} alt="Projets" />
+				<li className="header-logo-projects-container">
+					<img className="header-logo-projects" src={projects} alt="Projets" />
 					<p>Projets</p>
 				</li>
-				<li>
-					<img src={contact} alt="Contact" />
+				<li className="header-logo-contact-container">
+					<img className="header-logo-contact" src={contact} alt="Contact" />
 					<p>Contact</p>
 				</li>
 			</nav>
